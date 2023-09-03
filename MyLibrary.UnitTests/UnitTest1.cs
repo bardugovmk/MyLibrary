49e251e2c6f59674ee6dc359817c720e28f9ce4a
MIKHAIL BARDZIUHOU <91759981+bardugovmk@users.noreply.github.com>
2023-09-03 15:55:52 +0300

    Добавьте файлы проекта.

---
+++ MyLibrary.UnitTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace MyLibrary.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ MyLibrary/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace MyLibrary
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_5(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_6(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
